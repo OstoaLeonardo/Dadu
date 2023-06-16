369bee982d8ce5ce2f42b17b277f33ad1d0dc65f
LAG17 <amadorluis17172@gmail.com>
2023-06-15 20:46:01 -0600

    APP de Tareas incluida

--- app/src/main/java/com/example/dadu/ToDoFragment.java
+++ app/src/main/java/com/example/dadu/ToDoFragment.java
@@ -1,21 +1,31 @@
 package com.example.dadu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoFragment extends Fragment {
+    List<String> data = new ArrayList<>();
 
+    RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -42,7 +52,54 @@ public class ToDoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_to_do, container, false);
+        data.add("Elemento 1");
+        data.add("Elemento 2");
+        data.add("Elemento 3");
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        adapter = new RecyclerViewAdapter(requireContext(), data);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fabButton = view.findViewById(R.id.fab_btn_add);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                builder.setTitle("Agregar tarea");
+
+                final EditText input = new EditText(requireContext());
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newItemDescription = input.getText().toString();
+
+                        data.add(newItemDescription);
+                        adapter.notifyItemInserted(data.size() - 1);
+
+                        Toast.makeText(requireContext(), "Tarea agregada", Toast.LENGTH_SHORT).show();
+
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+            }
+        });
 
         return view;
     }
+
+
 }
